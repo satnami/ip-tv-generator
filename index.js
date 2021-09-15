a7f424320824207199a37fcb0526a2a301eae2fa
Mhd Sami Almouhtaseb <mssatnami@gmail.com>
2021-09-15 12:34:46 +0800

    feature, add cache, lint

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ const express = require('express')
 const request = require('request')
 const async = require('async')
 const app = express()
-const port = process.env.PORT || '3000'
 
+const port = process.env.PORT || '3000'
+const redis_url = process.env.REDIS_URL || 'redis://localhost:6379'
 const countries = ['ae', 'kw', 'ps', 'bh', 'eg', 'lb', 'qa', 'sa', 'sy']
+
+const cache = require('express-redis-cache')({
+  client: require('redis').createClient(redis_url)
+})
+
 function callback(error, response, body, cb) {
-  if(error || response.statusCode != 200)
+  if (error || response.statusCode != 200)
     return cb(true);
 
   cb(null, body);
 }
 
 let tasks = {};
-countries.forEach(function(country){
+countries.forEach(function (country) {
   tasks[country] = function (cb) {
-    request({url: "https://raw.githubusercontent.com/iptv-org/iptv/master/channels/"+country+".m3u"}, function (error, response, body) {
+    request({
+      url: "https://raw.githubusercontent.com/iptv-org/iptv/master/channels/" + country + ".m3u"
+    }, function (error, response, body) {
 
       callback(error, response, body.split('\n'), cb);
     });
@@ -23,13 +31,13 @@ countries.forEach(function(country){
 })
 
 
-app.get('/', (req, res) => {
-  // res.json({1:2})
+app.get('/', cache.route({ expire: 10 }), (req, res) => {
   async.parallel(tasks, function (err, resp) {
-    if(err) {
+    if (err) {
+      res.send("");
       return;
     }
-  
+
     let data = []
     Object.values(resp).forEach(function (value) {
       data = data.concat(value);
